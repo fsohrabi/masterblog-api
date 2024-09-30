bf0d9c7309d1cbfe9e0d38a617ea0febc1fde393
fsohrabi <fs.sohrabi70@gmail.com>
2024-09-30 14:40:12 +0200

    the message for delete post has changed

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -36,8 +36,7 @@ def delete(post_id):
     Route for deleting a blog post by its post ID.
     """
     storage.delete_blog(post_id)
-    posts = storage.list_blogs()
-    return jsonify(posts)
+    return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 200
 
 
 if __name__ == '__main__':
